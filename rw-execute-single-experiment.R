3012c9d0de183e98a9b2ca2c10de80ed764606b4
Kaleigh Clary <kclary@cs.umass.edu>
2021-10-11 23:58:04 -0700

    refactor

--- rw-execute-single-experiment.R
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env Rscript
-args = commandArgs(trailingOnly=TRUE)
-
-source("test-suite.R")
-
-configs <- read.csv("configs/all_adv_configurations_rw.csv")
-test.single.config(args[[1]], configs, trials=10, all=FALSE)
